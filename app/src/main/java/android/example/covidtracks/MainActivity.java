66a236e5e3bc2ac3d379dec03dd0ea221e387333
Isha <iv16182@gmail.com>
2021-06-02 11:09:50 +0530

    next update

--- app/src/main/java/android/example/covidtracks/MainActivity.java
+++ app/src/main/java/android/example/covidtracks/MainActivity.java
@@ -1,11 +1,16 @@
 package android.example.covidtracks;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.view.textclassifier.ConversationActions;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
@@ -25,16 +30,47 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
+    private ListView listView;
+    private DataAdapter dataAdapter;
+    private TextView confirmedC;
+    private TextView recoveredC;
+    private TextView deathC;
+    private RequestQueue requestQueue;
+    View loadingIndicator;
+    private TextView emptyText;
+
     private final static String REQUEST_URL = "https://api.covid19india.org/data.json";
-    ArrayList<DataCovid> StateWiseList = new ArrayList<DataCovid>();
+    List<DataCovid> StateWiseList = new ArrayList<DataCovid>();
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        RequestQueue requestQueue;
-        requestQueue = Volley.newRequestQueue(this);
+        emptyText = findViewById(R.id.empty_view);
+        View loadingIndicator = findViewById(R.id.loading_indicator);
+
+        requestQueue = requestSingleton.getInstance(this).getmRequestQueue();
+
+        Log.d("myapp","length : "+StateWiseList.size());
+
+        dataAdapter = new DataAdapter(this, StateWiseList);
+
+        listView = findViewById(R.id.list);
+        listView.setEmptyView(emptyText);
+        Log.d("myapp","length : "+StateWiseList.size());
+        jsonParsing();
+        loadingIndicator.setVisibility(View.GONE);
+    }
+
+    public void setDataAdapter(){
+
+        listView.setAdapter(dataAdapter);
+    }
+
+    private void jsonParsing(){
 
         JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, REQUEST_URL, null, new Response.Listener<JSONObject>() {
             @Override
@@ -48,9 +84,21 @@ public class MainActivity extends AppCompatActivity {
                         String recovered = stateA.getString("recovered");
                         String death = stateA.getString("deaths");
                         StateWiseList.add(new DataCovid(state,confirmed,recovered,death));
-                        Log.d("myapp",state +" "+confirmed+" "+death);
+                        Log.d("myapp",state +" "+confirmed+" "+death +" "+StateWiseList.size());
                     }
 
+                    JSONObject country = statewise.getJSONObject(0);
+                    String dConfirmed = country.getString("confirmed");
+                    String dRecovered = country.getString("recovered");
+                    String dDeath  = country.getString("deaths");
+                    confirmedC = findViewById(R.id.confirmedC);
+                    confirmedC.setText(dConfirmed);
+                    recoveredC = findViewById(R.id.recoveredC);
+                    recoveredC.setText(dRecovered);
+                    deathC = findViewById(R.id.deathC);
+                    deathC.setText(dDeath);
+
+                    setDataAdapter();
 
                 } catch (JSONException e) {
                     Log.d("myapp","error in json");
@@ -62,47 +110,7 @@ public class MainActivity extends AppCompatActivity {
                 Log.d("myapp","error in establishing connection");
             }
         });
-
         requestQueue.add(jsonObjectRequest);
-
-        Log.d("myapp","length : "+StateWiseList.size());
-
-        DataAdapter dataAdapter = new DataAdapter(this, StateWiseList);
-        ListView listView = findViewById(R.id.list);
-
-        listView.setAdapter(dataAdapter);
-        Log.d("myapp","length : "+StateWiseList.size());
-
     }
-
-//    public ArrayList<DataCovid> json(){
-//        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, REQUEST_URL, null, new Response.Listener<JSONObject>() {
-//            @Override
-//            public void onResponse(JSONObject response) {
-//                try {
-//                    JSONArray statewise = response.getJSONArray("statewise");
-//                    for (int i = 1 ; i<statewise.length();i++){
-//                        JSONObject stateA = statewise.getJSONObject(i);
-//                        String state = stateA.getString("state");
-//                        String confirmed = stateA.getString("confirmed");
-//                        String recovered = stateA.getString("recovered");
-//                        String death = stateA.getString("deaths");
-//                        StateWiseList.add(new DataCovid(state,confirmed,recovered,death));
-//                        Log.d("myapp",state +" "+confirmed+" "+death);
-//                    }
-//
-//
-//                } catch (JSONException e) {
-//                    Log.d("myapp","error in json");
-//                }
-//            }
-//        }, new Response.ErrorListener() {
-//            @Override
-//            public void onErrorResponse(VolleyError error) {
-//                Log.d("myapp","error in establishing connection");
-//            }
-//        });
-//
-//    }
 
 }
---
+++ app/src/main/java/android/example/covidtracks/requestSingleton.java
@@ -0,0 +1,28 @@
+package android.example.covidtracks;
+
+import android.content.Context;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.Volley;
+
+public class requestSingleton {
+    // only one oject of this class can be made // can be used for th elifetime of our app
+    private static  requestSingleton instance;
+    private RequestQueue mRequestQueue;
+
+    private requestSingleton(Context context){
+         mRequestQueue = Volley.newRequestQueue(context.getApplicationContext());
+    }
+
+    public static synchronized  requestSingleton getInstance(Context context){
+        if (instance == null){
+            instance = new requestSingleton(context);
+        }
+        return instance;
+    }
+
+    public RequestQueue getmRequestQueue(){
+        return mRequestQueue;
+    }
+
+}
